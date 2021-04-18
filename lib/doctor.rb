37f5a91b4465f48645004ef400253d39994cb9f9
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 18:05:04 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+  @@all = []
+  attr_accessor :name, :appointments, :patient, :date
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def appointments
+Appointment.all.select do |apppointments| appointments.doctor = self
+end
+  end
+  def new_appointment(date, patient)
+   Appointment.new(date, patient)
+  end
+  def patients
+    @appointments.collect do |appointments|
+      appointment.patient
+    end
+  end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,27 @@
+class Patient
+  attr_accessor :name, :date, :doctor
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, doctor)
+  end
+
+  def self.all
+   @@all
+  end
+
+  def qppointments
+    Appointment.all.select do |appoimntment|
+      appointment.patient == self
+    end
+end
+    def doctors
+      appointment.map do |appointment|
+        appointment.doctor
+      end
+    end
+end
