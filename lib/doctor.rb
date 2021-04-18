58a8e19de4102d5913b369d22edb0723c143cfa7
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 18:12:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -3,7 +3,7 @@ class Doctor
   attr_accessor :name, :appointments, :patient, :date
   def initialize(name)
     @name = name
-    @@all << self
+    doctor.all << self
   end
   def appointments
 Appointment.all.select do |apppointments| appointments.doctor = self
