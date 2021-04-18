01b3451e9a5d23edeab313cb38a4b20d534c5fab
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 18:22:40 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,10 +18,10 @@ end
 #####
   end
   def new_appointment(date, patient)
-   Appointment.new(date, patient)
+   Appointment.new(date, patient, self)
   end
   def patients
-    @appointments.collect do |appointment|
+    @appointment.collect do |appointment|
       appointment.patient
     end
   end
