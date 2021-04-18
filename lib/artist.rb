e6d8d4288caac08ea9fe40986194313bff0ebbff
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-04-18 18:06:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
     Song.all.select do |song| song.artist == self
     end
   end
-  def new_song(name, artist, genre)
-  Song.new(name, artist, genre) == self
+  def new_song(name, genre)
+  Song.new(name, self, genre) #== self
   end
 def genres
 self.songs.map {|song| song.genre}
